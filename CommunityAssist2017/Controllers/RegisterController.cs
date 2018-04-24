8679da143d0175c0d53ec0b04831de14e262d6e5
Judas, Benjamin <Benjamin.Judas@seattlecolleges.edu>
2018-04-24 12:33:36 -0700

    Register Push - needs review; Routing to register from Layout not working.

---
+++ CommunityAssist2017/Controllers/RegisterController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist2017.Models;
+
+namespace CommunityAssist2017.Controllers
+{
+    public class RegisterController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include ="LastName," +
+            "FirstName," +
+            "Email," +
+            "Password," +
+            "ApartmentNumber," +
+            "Street," +
+            "City," +
+            "State," +
+            "Zipcode," +
+            "Phone")]NewPerson np)
+        {
+            Message msg = new Message();
+            int result = db.usp_Register(np.LastName, np.FirstName, np.Email, np.PlainPassword, np.Apartment,
+                np.Street, np.City, np.State, np.Zipcode, np.Phone);
+            if (result != -1)
+            {
+                msg.MessageText = "Welcome! Congrats on Registering, " + np.FirstName + " " + np.LastName;
+
+            } 
+            else
+            {
+                msg.MessageText = "Something went wrong! You need to try again. Do better! Be better!";
+            }
+
+            return View("Result", msg);
+        }
+
+
+
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+            
+    }
+}
